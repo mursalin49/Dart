d87497efcb1bb634cc222c09f786d342958e8a6f
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-12 12:38:29 +0600

    Add files via upload

---
+++ syntax_&_datatypes.dart
@@ -0,0 +1,20 @@
+void main(){
+  int age =30;  // integer data type 
+  print(age);
+  double a = 2.0;   // double data type
+  print(a);
+  String name = 'Nirob Hasan'; //string data type
+  print(name);
+  bool value = true; //boolean data type
+  print(value);
+  List mylist = ['mursalin','hasan','nirob'];
+  print(mylist);
+  Map mymap = {
+    'name':'nirob',
+    'age' : 22,
+    'id' : 21225103423
+  };
+  print(mymap);
+  Set myset ={'mursalin','hasan','nirob'};
+  print(myset);
+}
