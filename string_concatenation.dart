6bdbbc47bf88d8e6c11c974ee3c476d2f1c031e2
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-13 00:36:05 +0600

    Add files via upload

---
+++ string_concatenation.dart
@@ -0,0 +1,17 @@
+void main() {
+  String a = 'we ';
+  String b = 'are ';
+  String c = 'learning ';
+  String d = 'dart';
+
+   print(a + b + c + d);
+
+  print('$a$b$c$d');
+
+  
+  String e = 'we ' 'are ' 'learning ' 'dart';
+  print(e);
+
+  List<String> f = ['we', 'are', 'learning', 'dart'];
+  print(f);
+}
