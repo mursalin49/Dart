7a790b8ea274654ee22f1331b563643248b42758
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-15 10:54:02 +0600

    Add files via upload

---
+++ final vs const.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 10;
+  int b = 20;
+  //final 
+  final int age = a;
+  print(age);
+  //const
+  const b = 20; // error output  show
+  print(b);
+   const double pi = 3.1416; // right output show
+   print(pi);
+}
