7e898b00d1b07227e76787cfd3fb23f9c979df11
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-16 18:01:04 +0600

    Add files via upload

---
+++ map.dart
@@ -0,0 +1,12 @@
+void main(){
+  var m = { 1:2 , 5:6, 8:9};
+  print(m);
+  print(m[1]);
+  print(m.keys);
+  print(m.values);
+  var n = {"name": "Nirob", "id": 23, "age": 22};
+  print(n.containsKey("name"));
+  print(n.containsValue("Nirob"));
+  print(n.remove("id"));
+
+}
