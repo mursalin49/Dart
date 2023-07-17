47d7277173005a63b5f4a5e79feb5ae14c87c6b2
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-17 22:45:52 +0600

    Add files via upload

---
+++ loops.dart
@@ -0,0 +1,30 @@
+void main() {
+ /* for loop
+  for(var i=0;i<10;i++ ){
+     // print(" ${i} Nirob"); // output: (0-9) Nirob
+    
+     /*if(i==4){    
+         continue;
+       }  //  output: skip 4 
+        print(i);
+        
+        */
+        if(i==8){
+          break;
+        }
+   print(i); // output: (0-7)
+  } */
+
+   /* while loop start 
+   var i=0;
+   while(i<10){
+    print(i);
+    i++; */
+     // do while loop start 
+     var i=0;
+     do{
+      print(i);
+    i++;
+     }while(i<10);
+   }
+  
