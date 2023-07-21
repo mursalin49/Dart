bde3655f7b34d858b3691cdefe8462ffd1ffce57
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-21 16:34:43 +0600

    Add files via upload

---
+++ OOP/inheritance.dart
@@ -0,0 +1,17 @@
+void main(){
+var son = Son();
+son.methodTwo();
+}
+class Father{
+  int age =50;
+  methodOne(){
+    print('this is method 1');
+
+  }
+  methodTwo(){
+    print('this is method 2');
+  }
+}
+class Son extends Father{
+  
+}
